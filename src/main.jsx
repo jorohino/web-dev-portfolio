8284981424bce4bfd90e516a4200b62c173f31ad
jorohino <149121790+jorohino@users.noreply.github.com>
2025-01-21 16:30:43 -0500

    Fix main.jsx React imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App";
